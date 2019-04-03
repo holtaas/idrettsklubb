6f745aa9afb970a29e5127d2b7151ec6f64e1db4
Teodor Holt <teodor@Teodors-MacBook-Pro.local>
2019-04-03 11:07:24 +0200

    asd

--- omOss.php
+++ omOss.php
@@ -22,8 +22,33 @@
 	
 	<!-- Wrapper starts -->
 	<div class="wrapper">
-		<h1 class="overskrift1OmOss">Overskrift 1</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend tellus non nunc efficitur, at mollis tortor pharetra. Fusce a turpis et nulla faucibus auctor. Mauris pulvinar efficitur mollis. Aliquam elementum ante hendrerit nunc vulputate vestibulum nec ac leo. In nec dolor fermentum, finibus arcu et, fermentum dui. Praesent sit amet velit aliquam, feugiat urna a, eleifend mauris. Vivamus mattis tincidunt justo vitae blandit. Donec vitae justo a justo dictum semper eu sit amet mi. Duis turpis nulla, tempus ut massa sit amet, semper ornare dui. Ut venenatis maximus lorem sit amet consequat. Proin a finibus massa.</p>
+		<h1 class="overskrift1OmOss">Trenere</h1>
+		<table class="Trenere">
+			<tr>
+				<th>Tittel</th>
+				<th>Navn</th>
+				<th>Beltegrad</th>
+				<th>Bilde</th>
+			</tr>
+			<tr>
+				<td>Master</td>
+				<td>Siva Vadi</td>
+				<td>1. Dan</td>
+				<td><img src="src/" alt="missing"></td>
+			</tr>
+			<tr>
+				<td>Grand Master</td>
+				<td>Danny A. Huertas</td>
+				<td>8. Dan</td>
+				<td><img src="src/Danny.jpeg" alt="missing"></td>
+			</tr>
+			<tr>
+				<td>Ingen tittel</td>
+				<td>Stein Åke Bjørk</td>
+				<td>Gult</td>
+				<td><img src="src/Ake.jpeg" alt="missing"></td>
+			</tr>
+		</table>
 		
 		<img class="picture1Historie" src="src/picture3.jpg" alt="missing">
 	
